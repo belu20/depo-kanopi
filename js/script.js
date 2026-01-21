7f953d82bbd9bc026a90cebcbe0c4edaee440672
Belu20 <isanseptian20@gmail.com>
2026-01-21 20:36:04 +0700

    schema

--- js/script.js
+++ js/script.js
@@ -124,14 +124,3 @@ questions.forEach((question) => {
     }
   });
 });
-
-// loading
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-  loader.style.opacity = "0";
-  loader.style.pointerEvents = "none";
-
-  setTimeout(() => {
-    loader.style.display = "none";
-  }, 5000);
-});
